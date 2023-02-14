c425d7af3056b74654b7c9d1a358133f63568549
Vlad Ursu <vlad.ursu@atlasai.co.uk>
2023-02-14 14:17:50 +0000

    Custom logger and make handler work on manual QA example

--- mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
+++ mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
@@ -1,9 +1,52 @@
 from typing import Optional
 import os
 import pandas as pd
+import numpy as np
 import autokeras as ak
 from mindsdb.integrations.libs.base import BaseMLEngine
 from tensorflow.keras.models import load_model
+
+import logging
+
+def configure_logger(log_file: str = "ml_handler_logs.txt") -> None:
+    """Configure all the options for an app-wide logger.
+
+    Parameters
+    ----------
+    log_file : str
+        Path to the file where logs will be written to. File mode is set to append.
+    """
+
+    logging.basicConfig(
+        format="%(asctime)s  %(levelname)s  %(message)s",
+        filename=log_file,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filemode="a+",
+        level=logging.INFO,
+    )
+    return logging.getLogger("atlas-remote-logger")
+
+def create_logger(log_file: str = "ml_handler_logs.txt", log_name: str = "mindsdb-logger-x") -> logging.Logger:
+    """Configure and create a local logger.
+
+    Returns
+    -------
+    logger : logging.Logger
+        The pre-configured logger.
+    """
+
+    formatter = logging.Formatter("%(asctime)s  %(levelname)s  %(message)s")
+    handler = logging.FileHandler(log_file, mode="a+")
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(log_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return logger
+
+
+logger = create_logger()
+
+
 
 # Makes this run on Windows Subsystem for Linux
 os.environ["XLA_FLAGS"]="--xla_gpu_cuda_data_dir=/usr/lib/cuda"
@@ -19,15 +62,41 @@ DEFAULT_TRIALS = 3
 def train_autokeras_model(df, target, mode=DEFAULT_MODE):
     training_df = df.drop(target, axis=1)
     trainer = trainer_dict[mode](overwrite=True, max_trials=DEFAULT_TRIALS)
+
+    logger.info("In train(): before training")
+    logger.info(df)
+    logger.info(target)
+    logger.info(training_df)
+
+    training_df = pd.get_dummies(training_df)
+    logger.info("In train(): after getting dummies")
+    logger.info(training_df)
+    logger.info(training_df.columns.values.tolist())
+
     trainer.fit(training_df, df[target], epochs=DEFAULT_EPOCHS)
-    return trainer.export_model()
+    return trainer.export_model(), training_df.columns.values.tolist()
 
 
-def get_preds_from_autokeras_model(df, model, target):
+def get_preds_from_autokeras_model(df, model, target, all_column_names):
     cols_to_drop = ["__mindsdb_row_id", target]
     for col in cols_to_drop:
-        if col in df.columns:
+        if col in df.columns.values.tolist():
             df = df.drop(col, axis=1)
+    logger.info("In get_preds(): before getting dummies")
+    logger.info(df)
+    logger.info(target)
+    logger.info("In get_preds(): after getting dummies")
+    df = pd.get_dummies(df)
+    logger.info(df)
+    logger.info("In get_preds(): before filling missing columns")
+    logger.info(all_column_names)
+    logger.info(df.columns.values.tolist())
+    for col in all_column_names:
+        if col not in df.columns.values.tolist():
+            df[col] = 0
+    logger.info("In get_preds(): after filling missing columns")
+    logger.info(df)
+
     return model.predict(df)
 
 
@@ -47,8 +116,12 @@ class AutokerasHandler(BaseMLEngine):
         args["target"] = target
         args["folder_path"] = "autokeras"
 
-        
-        model = train_autokeras_model(df, target)
+        logger.info("In create(): Before training")
+        logger.info(df)
+        logger.info(df.dtypes)
+        logger.info(df.shape)
+        logger.info(target)
+        model, args["data_column_names"] = train_autokeras_model(df, target)
 
         model.save(args["folder_path"])
         self.model_storage.json_set("predict_args", args)
@@ -56,9 +129,14 @@ class AutokerasHandler(BaseMLEngine):
     
     def predict(self, df, args=None):
         args = self.model_storage.json_get("predict_args")
+        logger.info("Before load model")
         model = load_model(args["folder_path"], custom_objects=ak.CUSTOM_OBJECTS)
 
         df_to_predict = df.copy()
-        predictions = get_preds_from_autokeras_model(df_to_predict, model, args["target"])
+        logger.info(df_to_predict)
+        logger.info(df_to_predict.dtypes)
+        logger.info(df.shape)
+        logger.info("Before get predictions")
+        predictions = get_preds_from_autokeras_model(df_to_predict, model, args["target"], args["data_column_names"])
         df_to_predict[args["target"]] = predictions
         return df_to_predict
