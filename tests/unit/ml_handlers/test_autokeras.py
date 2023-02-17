e27b30e60ebe22612243a23e3a75169e45a58a83
Vlad Ursu <vlad.ursu@atlasai.co.uk>
2023-02-17 11:07:25 +0000

    Add more tests

--- tests/unit/ml_handlers/test_autokeras.py
+++ tests/unit/ml_handlers/test_autokeras.py
@@ -109,6 +109,49 @@ class TestAutokeras(BaseExecutorTest):
         # value is around 1
         assert (avg_c > -5) and (avg_c < 5)
 
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_regression_with_nulls_in_training_data(self, mock_handler):
+
+        # dataset, string values
+        df = pd.DataFrame(range(1, 50), columns=["a"])
+        df["b"] = 50 - df.a
+        df["c"] = round((df["a"] * 3 + df["b"]) / 50)
+        df["d"] = np.where(df.index % 2, "even", "odd")
+        # Make it look like we have missing data
+        df["a"][10] = np.nan
+        df["b"][25] = np.nan
+        df["d"][31] = ""
+
+        self.set_handler(mock_handler, name="pg", tables={"df": df})
+
+        # create project
+        self.run_sql("create database proj")
+
+        # create predictor
+        self.run_sql(
+            """
+           create model proj.modelx
+           from pg (select * from df)
+           predict c
+           using 
+             engine='autokeras';
+        """
+        )
+        self.wait_predictor("proj", "modelx")
+
+        # run predict
+        ret = self.run_sql(
+            """
+           SELECT c
+           FROM proj.modelx
+           WHERE a=1
+           AND d="odd";
+        """
+        )
+        avg_c = pd.to_numeric(ret.c).mean()
+        # value is around 1
+        assert (avg_c > -5) and (avg_c < 5)
+
     @pytest.mark.skip("Threading issues not investigated yet")
     @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
     def test_regression_with_bulk_predict_query(self, mock_handler):
@@ -263,6 +306,47 @@ class TestAutokeras(BaseExecutorTest):
 
         assert ret.d[0][0] in ["even", "odd"]
 
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_classification_with_nulls_in_training_data(self, mock_handler):
+
+        # dataset, string values
+        df = pd.DataFrame(range(1, 50), columns=["a"])
+        df["b"] = 50 - df.a
+        df["d"] = np.where(df.index % 2, "even", "odd")
+        df["e"] = np.where(df.index % 3, "not_prime", "kinda_prime")
+        # Make it look like we have missing data
+        df["a"][10] = np.nan
+        df["b"][25] = np.nan
+        df["e"][31] = ""
+
+        self.set_handler(mock_handler, name="pg", tables={"df": df})
+
+        # create project
+        self.run_sql("create database proj")
+
+        # create predictor
+        self.run_sql(
+            """
+           create model proj.modelx
+           from pg (select * from df)
+           predict d
+           using 
+             engine='autokeras';
+        """
+        )
+        self.wait_predictor("proj", "modelx")
+
+        # run predict
+        ret = self.run_sql(
+            """
+           SELECT d
+           FROM proj.modelx
+           WHERE a=1
+           AND e="kinda_prime";
+        """
+        )
+
+        assert ret.d[0][0] in ["even", "odd"]
 
     @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
     def test_classification_error_on_predict_query_too_strict(self, mock_handler):
@@ -303,3 +387,42 @@ class TestAutokeras(BaseExecutorTest):
             assert False
         except Exception:
             assert True
+
+    @pytest.mark.skip("Can't figure out how to assert it")
+    @patch("mindsdb.integrations.handlers.postgres_handler.Handler")
+    def test_classification_with_bulk_predict_query(self, mock_handler):
+
+        # dataset, string values
+        df = pd.DataFrame(range(1, 50), columns=["a"])
+        df["b"] = 50 - df.a
+        df["c"] = round((df["a"] * 3 + df["b"]) / 50)
+        df["d"] = np.where(df.index % 2, "even", "odd")
+
+        self.set_handler(mock_handler, name="pg", tables={"df": df})
+
+        # create project
+        self.run_sql("create database proj")
+
+        # create predictor
+        self.run_sql(
+            """
+           create model proj.modelx
+           from pg (select * from df)
+           predict d
+           using 
+             engine='autokeras';
+        """
+        )
+        self.wait_predictor("proj", "modelx")
+
+        # run predict
+        ret = self.run_sql(
+            """
+            SELECT t.d, m.d, t.a, t.b
+            FROM pg.df as t 
+            JOIN proj.modelx as m limit 10;
+        """
+        )
+
+        for i, row in enumerate(ret.d):
+            assert ret.d[i][0] in ["even", "odd"]
