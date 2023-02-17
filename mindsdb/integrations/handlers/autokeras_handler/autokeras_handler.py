7ba7740d056612d6c080fe0b69b2f9b7f5f12884
Vlad Ursu <vlad.ursu@atlasai.co.uk>
2023-02-17 14:28:33 +0000

    Remove custom logger, refactor

--- mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
+++ mindsdb/integrations/handlers/autokeras_handler/autokeras_handler.py
@@ -7,47 +7,7 @@ from sklearn import preprocessing
 from mindsdb.integrations.libs.base import BaseMLEngine
 from tensorflow.keras.models import load_model
 
-import logging
-
-def configure_logger(log_file: str = "ml_handler_logs.txt") -> None:
-    """Configure all the options for an app-wide logger.
-
-    Parameters
-    ----------
-    log_file : str
-        Path to the file where logs will be written to. File mode is set to append.
-    """
-
-    logging.basicConfig(
-        format="%(asctime)s  %(levelname)s  %(message)s",
-        filename=log_file,
-        datefmt="%Y-%m-%d %H:%M:%S",
-        filemode="a+",
-        level=logging.INFO,
-    )
-    return logging.getLogger("atlas-remote-logger")
-
-def create_logger(log_file: str = "ml_handler_logs.txt", log_name: str = "mindsdb-logger-x") -> logging.Logger:
-    """Configure and create a local logger.
-
-    Returns
-    -------
-    logger : logging.Logger
-        The pre-configured logger.
-    """
-
-    formatter = logging.Formatter("%(asctime)s  %(levelname)s  %(message)s")
-    handler = logging.FileHandler(log_file, mode="a+")
-    handler.setFormatter(formatter)
-    logger = logging.getLogger(log_name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    return logger
-
-
-logger = create_logger()
-
-
+from mindsdb.utilities import log
 
 # Makes this run on Windows Subsystem for Linux
 os.environ["XLA_FLAGS"]="--xla_gpu_cuda_data_dir=/usr/lib/cuda"
@@ -61,6 +21,7 @@ DEFAULT_EPOCHS = 3
 DEFAULT_TRIALS = 3
 
 def train_autokeras_model(df, target):
+    # Choose regressor of classifier based on target data type
     if np.issubdtype(df[target].dtype, np.number):
         mode = "regression"
         y_train = df[target]
@@ -72,43 +33,30 @@ def train_autokeras_model(df, target):
     training_df = df.drop(target, axis=1)
     trainer = trainer_dict[mode](overwrite=True, max_trials=DEFAULT_TRIALS)
 
-    logger.info("In train(): before training")
-    logger.info(df)
-    logger.info(target)
-    logger.info(training_df)
-
+    # Save the column names of all numeric columns before transforming any categorical columns into dummies
     numeric_column_names = training_df.select_dtypes(include=[np.number]).columns.values.tolist()
 
     training_df = pd.get_dummies(training_df)
-    logger.info("In train(): after getting dummies")
-    logger.info(training_df)
-    logger.info(training_df.columns.values.tolist())
 
+    # Extract all dummy column names. These will be used to prepare the predict df before the prediction
     categorical_dummy_column_names = [col for col in training_df.columns.values.tolist() if col not in numeric_column_names]
 
     trainer.fit(training_df, y_train, epochs=DEFAULT_EPOCHS)
     return trainer.export_model(), categorical_dummy_column_names
 
 
-def get_preds_from_autokeras_model(df, model, target, all_column_names):
+def get_preds_from_autokeras_model(df, model, target, categorical_dummy_column_names):
+    # Remove columns that didn't exist in the training df
     cols_to_drop = ["__mindsdb_row_id", target]
     for col in cols_to_drop:
         if col in df.columns.values.tolist():
             df = df.drop(col, axis=1)
-    logger.info("In get_preds(): before getting dummies")
-    logger.info(df)
-    logger.info(target)
-    logger.info("In get_preds(): after getting dummies")
+
+    # Get dummies for any categorical columns and then populate the missing ones with zeros
     df = pd.get_dummies(df)
-    logger.info(df)
-    logger.info("In get_preds(): before filling missing columns")
-    logger.info(all_column_names)
-    logger.info(df.columns.values.tolist())
-    for col in all_column_names:
+    for col in categorical_dummy_column_names:
         if col not in df.columns.values.tolist():
             df[col] = 0
-    logger.info("In get_preds(): after filling missing columns")
-    logger.info(df)
 
     return model.predict(df)
 
@@ -128,65 +76,49 @@ class AutokerasHandler(BaseMLEngine):
         args = args['using']  # ignore the rest of the problem definition
         args["target"] = target
         args["folder_path"] = "autokeras"
+        # Save the training df in order to filter the training data based on the predict df
         args["training_df"] = df.to_json()
-
-        logger.info("In create(): Before training")
-        logger.info(df)
-        logger.info(df.dtypes)
-        logger.info(df.shape)
-        logger.info(target)
         args["training_data_column_count"] = len(df.columns) - 1 # subtract 1 for target
-        model, args["data_column_names"] = train_autokeras_model(df, target)
 
+        model, args["data_column_names"] = train_autokeras_model(df, target)
         model.save(args["folder_path"])
         self.model_storage.json_set("predict_args", args)
-
     
     def predict(self, df, args=None):
         args = self.model_storage.json_get("predict_args")
         training_df = pd.read_json(args["training_df"])
 
-        logger.info("Before load model")
         model = load_model(args["folder_path"], custom_objects=ak.CUSTOM_OBJECTS)
 
         df_to_predict = df.copy()
+        # Remove column that didn't exist in the training df so that we can do filtering
         if "__mindsdb_row_id" in df_to_predict.columns.values.tolist():
             df_to_predict = df_to_predict.drop("__mindsdb_row_id", axis=1)
 
+        # Filter the training df based on the predict df
         keys = list(df_to_predict.columns.values)
         i1 = training_df.set_index(keys).index
         i2 = df_to_predict.set_index(keys).index
         filtered_df = training_df[i1.isin(i2)]
+
         if filtered_df.empty:
             # TODO: Rephrase the exception message in a more user-friendly way
             raise Exception("The condition(s) in the WHERE clause filtered out all the data. Please refine these and try again")
-        logger.info(filtered_df)
-        logger.info(filtered_df.shape)
-        original_y = training_df[args["target"]]
-        logger.info("Before get predictions")
-        logger.info(f"original_y:\n{original_y}")
 
+        original_y = training_df[args["target"]]
         predictions = get_preds_from_autokeras_model(filtered_df, model, args["target"], args["data_column_names"])
-        logger.info(f"These are the raw predictions: {predictions}")
+
+        # If we used the classifier we need to pre-process the predictions before returning them
         if not np.issubdtype(original_y.dtype, np.number):
-            logger.info("INSIDE classifier predict")
+            # Turn prediction back into categorical value
             lb = preprocessing.LabelBinarizer()
-            logger.info(f"original_y to fit:\n{original_y}")
             lb.fit(original_y)
             preds = lb.inverse_transform(predictions)
-            logger.info(f"raw preds: {predictions}")
-            # return [(preds[i], max(row)) for i, row in enumerate(x)]
+
+            # Add the confidence score next to the prediction
             filtered_df[args["target"]] = [(preds[i], max(row)) for i, row in enumerate(predictions)]
-            logger.info(f"This is your prediction df: {filtered_df}")
-            logger.info(f"This is your prediction alone: {filtered_df[args['target']]}")
-            pred_val = filtered_df[args["target"]].apply(lambda x: x[0])
-            pred_conf = filtered_df[args["target"]].apply(lambda x: x[1])
-            logger.info(f"Pred val: {pred_val} | pred conf: {pred_conf}")
-            # for i, row in enumerate(filtered_df[args['target']]):
-            #     logger.info(f'my hack: {filtered_df[args["target"]][i][0] in ["even", "odd"]}')
             return filtered_df
 
 
         filtered_df[args["target"]] = predictions
-        logger.info(f"This is your prediction df: {filtered_df}")
         return filtered_df
